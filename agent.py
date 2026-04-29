b5cd3d0c0d2adde003e3fc1b66e3b2856b4ab591
kanna-07-rr <rohithreddy1293@gmail.com>
2026-04-30 02:10:20 +0530

    Run workflow

--- agent.py
+++ agent.py
@@ -14,7 +14,7 @@ res = requests.post(
         "Content-Type": "application/json"
     },
     json={
-        "model": "mistralai/mistral-7b-instruct:free",
+        "model": "google/gemma-3-4b-it:free",
         "messages": [
             {"role": "system", "content": SYSTEM},
             {"role": "user", "content": PROMPT}
